4cebb9c8030c75d68ed8c469a5ebde85b1b33c45
yongj.zhuang <yongj.zhuang@outlook.com>
2026-01-15 16:31:05 +0800

    Research Clarifier

---
+++ agents/deep_research_clarifier.go
@@ -0,0 +1,140 @@
+package agents
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/cloudwego/eino/components/model"
+	"github.com/cloudwego/eino/components/tool"
+	"github.com/cloudwego/eino/components/tool/utils"
+	"github.com/cloudwego/eino/compose"
+	"github.com/cloudwego/eino/schema"
+	"github.com/curtisnewbie/miso/errs"
+	"github.com/curtisnewbie/miso/flow"
+	"github.com/curtisnewbie/miso/util/strutil"
+)
+
+type DeepResearchClarifier struct {
+	genops *GenericOps
+	graph  compose.Runnable[DeepResearchClarifierInput, *DeepResearchClarifierOutput]
+}
+
+type DeepResearchClarifierInput struct {
+	Conversation string `json:"conversation"`
+	Memory       string `json:"memory"`
+}
+
+type DeepResearchClarifierOutput struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type DeepResearchClarifierOps struct {
+	genops *GenericOps
+
+	SystemMessagePrompt string
+
+	// Provided variables: ${conversation}, ${memory}
+	UserMessagePrompt string
+}
+
+func NewDeepResearchClarifierOps(g *GenericOps) *DeepResearchClarifierOps {
+	return &DeepResearchClarifierOps{
+		genops: g,
+		SystemMessagePrompt: `
+You are a research assistant, you are given a historical conversation between you and the user.
+Your task is to analyze the conversation, guess what are the research title and description that user wants, and use the tool 'FillResearchInfo' to fill in the fields.
+If you don't know what user wants, leave the field empty.
+The generated research title and description are mainly suggestion for user's convenience, user may modify them if necessary.
+`,
+		UserMessagePrompt: `
+<conversation>
+${conversation}
+</conversation>
+
+<memory>
+${memory}
+</memory>
+`,
+	}
+}
+
+func NewDeepResearchClarifier(rail flow.Rail, chatModel model.ToolCallingChatModel, ops *DeepResearchClarifierOps) (*DeepResearchClarifier, error) {
+
+	g := compose.NewGraph[DeepResearchClarifierInput, *DeepResearchClarifierOutput]()
+
+	_ = g.AddLambdaNode("prepare_messages", compose.InvokableLambda(func(ctx context.Context, in DeepResearchClarifierInput) ([]*schema.Message, error) {
+
+		systemMessage := schema.SystemMessage(strings.TrimSpace(ops.SystemMessagePrompt))
+		userMessage := schema.UserMessage(strings.TrimSpace(
+			strutil.NamedSprintf(ops.UserMessagePrompt, map[string]any{
+				"conversation": in.Conversation,
+				"memory":       in.Memory,
+			})),
+		)
+		rail.Debugf("System Message: %v", systemMessage.Content)
+		rail.Debugf("User Message: %v", userMessage.Content)
+
+		return []*schema.Message{
+			systemMessage,
+			userMessage,
+		}, nil
+	}), compose.WithNodeName("Prepare Messages"))
+
+	type FillResearchInfoInput struct {
+		Title       string `json:"title"`
+		Description string `json:"description"`
+	}
+
+	toolNode, err := compose.NewToolNode(context.TODO(), &compose.ToolsNodeConfig{
+		Tools: []tool.BaseTool{
+			utils.NewTool(
+				&schema.ToolInfo{
+					Name: "FillResearchInfo",
+					Desc: "Fill in Deep Research Title and Description",
+					ParamsOneOf: schema.NewParamsOneOfByParams(map[string]*schema.ParameterInfo{
+						"title": {
+							Type: "string",
+							Desc: "Research Title (less then 100 characters)",
+						},
+						"description": {
+							Type: "string",
+							Desc: "Research Description (less then 300 characters)",
+						},
+					}),
+				},
+				func(ctx context.Context, input FillResearchInfoInput) (output DeepResearchClarifierOutput, err error) {
+					return DeepResearchClarifierOutput(input), nil
+				}),
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	_ = g.AddToolsNode("tools", toolNode)
+	_ = g.AddChatModelNode("clarify_generate_research", chatModel, compose.WithNodeName("Generate Summary"))
+
+	_ = g.AddEdge(compose.START, "prepare_messages")
+	_ = g.AddEdge("prepare_messages", "clarify_generate_research")
+	_ = g.AddEdge("clarify_generate_research", "tools")
+	_ = g.AddEdge("tools", compose.END)
+
+	runnable, err := CompileGraph(rail, ops.genops, g, compose.WithGraphName("DeepResearchClarifier"))
+	if err != nil {
+		return nil, errs.Wrap(err)
+	}
+
+	return &DeepResearchClarifier{graph: runnable, genops: ops.genops}, nil
+}
+
+func (w *DeepResearchClarifier) Execute(rail flow.Rail, input DeepResearchClarifierInput) (*DeepResearchClarifierOutput, error) {
+	start := time.Now()
+	defer rail.TimeOp(start, "DeepResearchClarifier")
+
+	cops := []compose.Option{}
+	if w.genops.LogOnStart {
+		cops = append(cops, WithTraceCallback("DeepResearchClarifier", w.genops.LogInputs))
+	}
+	return w.graph.Invoke(rail, input, cops...)
+}
