c164e513155bce4098ccce7e97a8ffe4cd99982a
yongj.zhuang <yongj.zhuang@outlook.com>
2026-01-27 12:55:29 +0800

    Log token usage

--- agents/graph.go
+++ agents/graph.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/cloudwego/eino/callbacks"
+	"github.com/cloudwego/eino/components/model"
 	"github.com/cloudwego/eino/compose"
 	"github.com/curtisnewbie/miso/flow"
 )
@@ -23,12 +24,30 @@ func WithTraceCallback(name string, logInputs bool) compose.Option {
 		callbacks.NewHandlerBuilder().
 			OnStartFn(func(ctx context.Context, ri *callbacks.RunInfo, in callbacks.CallbackInput) context.Context {
 				if logInputs {
-					flow.NewRail(ctx).Infof("Graph exec %v, name: %v, type: %v, component: %v, input: %v", name, ri.Name, ri.Type, ri.Component, in)
+					flow.NewRail(ctx).Infof("Graph exec %v start, name: %v, type: %v, component: %v, input: %v", name, ri.Name, ri.Type, ri.Component, in)
 				} else {
-					flow.NewRail(ctx).Infof("Graph exec %v, name: %v, type: %v, component: %v", name, ri.Name, ri.Type, ri.Component)
+					flow.NewRail(ctx).Infof("Graph exec %v start, name: %v, type: %v, component: %v", name, ri.Name, ri.Type, ri.Component)
+				}
+				return ctx
+			}).
+			OnEndFn(func(ctx context.Context, ri *callbacks.RunInfo, output callbacks.CallbackOutput) context.Context {
+				inToken, outToken, ok := tokenUsage(output)
+				if ok {
+					flow.NewRail(ctx).Infof("Graph exec %v end, name: %v, type: %v, component: %v, usage: %v (input), %v (output)", name, ri.Name,
+						ri.Type, ri.Component, inToken, outToken)
 				}
 				return ctx
 			}).
 			Build(),
 	)
+}
+
+func tokenUsage(in callbacks.CallbackOutput) (_in int, _out int, ok bool) {
+	switch m := in.(type) {
+	case *model.CallbackOutput:
+		if m.TokenUsage != nil {
+			return m.TokenUsage.PromptTokens, m.TokenUsage.CompletionTokens, true
+		}
+	}
+	return 0, 0, false
 }
